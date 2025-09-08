4a21b0ff7025a8c966f73a7658dd291bd5e9cee5
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-09-08 21:55:08 +0400

    add logo

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -29,9 +29,9 @@ const Navigation = ({ theme, toggleTheme } : any) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex-shrink-0">
+          <div className="flex-shrink-0 ">
             <span className="text-2xl font-bold bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent">
-              Portfolio
+                <img src="/cover.svg" alt="" className='w-[150px] h-[150px] cursor-pointer'/>
             </span>
           </div>
 
